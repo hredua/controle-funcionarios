65e2ad20b4a3d6fdb9e12079dabcde8f3447a30b
Helamã Rédua <93559787+hredua@users.noreply.github.com>
2023-04-28 03:29:12 -0400

    update name

--- pages/employee/employee.js
+++ pages/employee/employee.js
@@ -30,7 +30,7 @@ function myScope() {
                 divEdit.classList.add('top')
                 contractModal.appendChild(divEdit)
                 const h2 = document.createElement('h2')
-                h2.innerHTML = `<b>${employee[i].name} ${employee[i].lastName}</b>`
+                h2.innerHTML = `<b>${employee[i].name}</b>`
                 const divEditDelete = document.createElement('div')
                 divEditDelete.classList.add('edit-delete')
                 const edit = document.createElement('i')
